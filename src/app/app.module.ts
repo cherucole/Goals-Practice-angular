0c83183257ebea9a22f28b7762a8b8dcaac1ccab
cherucole <cheruyotcheruyot@gmail.com>
2018-08-12 01:21:50 +0300

    working until delete goal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalComponent,
-    GoalDetailsComponent
+    GoalDetailsComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule
