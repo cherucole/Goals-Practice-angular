1b09d073404d34490db0e0defe1dd2e721312582
cherucole <cheruyotcheruyot@gmail.com>
2018-08-14 11:16:24 +0300

    added routing for the site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,28 @@ import {AlertsService} from './alert-service/alerts.service'
 import {HttpClientModule} from '@angular/common/http'
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+// Defining routes
+const routes:Routes=[
+  {path:"goals",component:GoalComponent},
+  {path:"about",component:AboutComponent},
+  {path:"",redirectTo:"/goals",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+
+  // {path:"form",component:GoalFormComponent}
+
+
+]
+
 
 @NgModule({
   declarations: [
@@ -18,7 +34,9 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalDetailsComponent,
     GoalComponent,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +44,7 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     HttpClientModule,
     NgProgressModule.forRoot(),// normal progress bar
    NgProgressHttpModule // progress bar to load http requests
+   RouterModule.forRoot(routes)
   ],
   providers: [AlertsService], // Add service to providers
   bootstrap: [AppComponent]
